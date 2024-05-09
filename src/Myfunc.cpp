7b2cb437e17336906a51c96f15fe870c0bd446fa
aikuLinuxCentos7 <2954558178@qq.com>
2024-05-09 22:27:42 +0800

    Revert "test 2"
    This reverts commit 22c02e391507161cbaf9af306b6a7c314b4ba508.

--- src/Myfunc.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Myfunc.h"
-using namespace std;
-void printInt(void *arg){
-        long long tmp = (long long)arg;
-        cout << "arg is " << tmp << "func is "  << __func__ << endl;
-}
-
--- src/Myfunc.h
+++
@@ -1,4 +0,0 @@
-#ifndef __MYFUNC_H__
-#define __MYFUNC_H__
-void printInt(void *arg);
-#endif
